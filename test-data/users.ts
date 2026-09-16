7486927a02a8e154029c3127286efe3aa58a1eda
karim selim <kareemsleem7@gmail.com>
2026-09-16 04:02:06 +0300

    test: add valid user data and signup page URL

---
+++ test-data/users.ts
@@ -0,0 +1,7 @@
+export const signupURL = "https://gitlab.com/users/sign_up";
+export const validUserSignup = {
+  firstName: "karim",
+  lastName: "selim",
+  username: "karim_selim",
+  email: "kareemsleem7@gmail.com",
+};
