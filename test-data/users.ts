ffd5db7eb26a501dba87cb04b6b9ed93e113a0de
karim selim <kareemsleem7@gmail.com>
2026-09-19 04:08:37 +0300

    chore: adding type for signup user

--- test-data/users.ts
+++ test-data/users.ts
@@ -1,7 +1,9 @@
+import { signupUser } from "../types/users";
+
 export const signupURL = "https://gitlab.com/users/sign_up";
-export const validUserSignup = {
-  firstName: "karim",
-  lastName: "selim",
+export const validUserSignup: signupUser = {
+  firstname: "karim",
+  lastname: "selim",
   username: "karim_selim",
   email: "kareemsleem7@gmail.com",
 };
---
+++ types/users.ts
@@ -0,0 +1,6 @@
+export type signupUser = {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+};
